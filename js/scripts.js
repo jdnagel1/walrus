ae08cbfa6052f8b533f3f02af25d4257cd1d2dd3
Gavin Kimberlin and Jake Nagel <jake@>
2019-03-26 11:21:29 -0700

    it will add a fade in toggle to text and image.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+  $(".clickable").click(function() {
+    $("#initially-showing").fadeToggle();
+    
+  });
+
+  $(".clickable").click(function() {
+    $("#initially-hidden").fadeToggle();
   });
 });
